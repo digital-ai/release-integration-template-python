95ecb5c11c57e2ef762f53f7f12539a14e56ff5b
Goran Pugar <goranpg@gmail.com>
2023-09-25 13:19:29 +0200

    S-95959 Use release-remote-runner instead of runner plugin

---
+++ dev-environment/digitalai-release-remote-runner/spin-remote-runner.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+MAX_WAIT_SECONDS=300
+WAIT_INTERVAL=5
+
+start_time=$(date +%s)
+end_time=$((start_time + MAX_WAIT_SECONDS))
+
+api_url="http://localhost:5516/tokens/users/remote-runner"
+unique_id=$(date +%s%N | md5sum | awk '{print $1}')
+
+while true; do
+    current_time=$(date +%s)
+
+    if [ $current_time -ge $end_time ]; then
+        echo "Timeout reached. Exiting!"
+        exit 1
+    fi
+
+    response=$(curl -s -i -X POST -u admin:admin -H "Content-Type: application/json;charset=UTF-8" -d '{"tokenNote": "'$unique_id'"}' $api_url)
+
+    if [ $? -ne 0 ]; then
+        echo "Fetching token failed - probably still initializing... retrying soon"
+        sleep $WAIT_INTERVAL
+        continue
+    fi
+
+    http_status=$(echo "$response" | awk 'NR==1{print $2}')
+    response_body=$(echo "$response" | sed -n '/^\r\{0,1\}$/,$p')
+    if [ $http_status -ge 200 ] && [ $http_status -lt 300 ]; then
+        echo "Successful fetched token..."
+        export RELEASE_RUNNER_TOKEN=$(echo "$response_body" | jq -r '.token')
+        break
+    else
+        echo "Fetching token - response had $http_status HTTP status code."
+        echo "Response body was: $response_body"
+    fi
+
+    echo "Error fetching token! Waiting $WAIT_INTERVAL seconds before trying again..."
+    sleep $WAIT_INTERVAL
+done
+
+exec /app/release-remote-runner --profiles docker
