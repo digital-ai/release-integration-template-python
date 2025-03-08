dc15e8577dac88a5806fa04291535b76d064c9b0
Balaji Venkatesan <balaji.venkatesan@digital.ai>
2025-03-08 21:37:13 +0530

    S-118621 Example for usage of ReleaseAPIClient

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 # Use the Python image as the base
-FROM python:alpine3.17
+FROM python:alpine3.21
 
 # Prevent Python from writing bytecode files and run in unbuffered mode
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
 
 # Create app folder and set permissions
 RUN mkdir /app && chmod -R 777 /app
--- src/sample_release_api_task.py
+++ src/sample_release_api_task.py
@@ -30,3 +30,44 @@ class SetSystemMessage(BaseTask):
 
         # Add a line to the comment section in the UI
         self.add_comment(f"System message updated to \"{message}\"")
+
+        # Example for usage of ReleaseAPIClient
+        release_api_client = self.get_release_api_client()
+
+        # Step 1: Create a new global variable
+        global_variable = {
+            "id": None,
+            "key": "global.newVar",
+            "type": "xlrelease.StringVariable",
+            "requiresValue": "false",
+            "showOnReleaseStart": "false",
+            "value": "new value"
+        }
+        response = release_api_client.post("/api/v1/config/Configuration/variables/global", json=global_variable)
+        response.raise_for_status()
+        global_variable_id = response.json()["id"]
+        print(f"Global variable created with ID: {global_variable_id}")
+
+        # Step 2: Update the global variable
+        update_global_variable = {
+            "id": global_variable_id,
+            "key": "global.newVar",
+            "type": "xlrelease.StringVariable",
+            "requiresValue": "false",
+            "showOnReleaseStart": "false",
+            "value": "updated value"
+        }
+        response = release_api_client.put(f"/api/v1/config/{global_variable_id}", json=update_global_variable)
+        response.raise_for_status()
+        print("Global variable updated:", response.json())
+
+        # Step 3: Retrieve the updated global variable
+        response = release_api_client.get(f"/api/v1/config/{global_variable_id}")
+        response.raise_for_status()
+        print("Retrieved global variable:", response.json())
+
+        # Step 4: Delete the global variable
+        response = release_api_client.delete(f"/api/v1/config/{global_variable_id}")
+        response.raise_for_status()
+        print(f"Global variable deleted successfully. Status code: {response.status_code}")
+
