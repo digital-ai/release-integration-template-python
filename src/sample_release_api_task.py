b809035626e6b2601a890e4d9600cb97770e97f4
Balaji Venkatesan <balaji.venkatesan@digital.ai>
2025-03-18 10:25:44 +0530

    -118621 Example for usage of ReleaseAPIClient

--- src/sample_release_api_task.py
+++ src/sample_release_api_task.py
@@ -1,11 +1,9 @@
 from digitalai.release.integration import BaseTask
-from digitalai.release.v1.api.configuration_api import ConfigurationApi
-from digitalai.release.v1.model.system_message_settings import SystemMessageSettings
 
 
 class SetSystemMessage(BaseTask):
     """
-        Sets the system message in the Release UI by invoking the API.
+        Sets the system message using the Release API client.
     """
 
     def execute(self) -> None:
@@ -13,26 +11,25 @@ class SetSystemMessage(BaseTask):
         # Get the message from the input properties
         message = self.input_properties['message']
 
-        # Create the REST client to connect to the Configuration API
-        configuration_api = ConfigurationApi(self.get_default_api_client())
+        # Obtain an instance of the Release API client
+        release_api_client = self.get_release_api_client()
 
-        # Define parameter object to send through the API client
-        system_message = SystemMessageSettings(
-            type='xlrelease.SystemMessageSettings',
-            id='Configuration/settings/SystemMessageSettings',
-            message=message,
-            enabled=True,
-            automated=False
-        )
+        # Prepare the payload for the API request
+        system_message = {
+            "type": "xlrelease.SystemMessageSettings",
+            "id" : "Configuration/settings/SystemMessageSettings",
+            "message": message,
+            "enabled": "True",
+            "automated": "False"
+        }
 
-        # Make the actual rest call to the designated endpoint
-        configuration_api.update_system_message(system_message_settings=system_message)
+        # Send a PUT request to update the system message configuration
+        release_api_client.put("/api/v1/config/system-message", json=system_message)
 
         # Add a line to the comment section in the UI
         self.add_comment(f"System message updated to \"{message}\"")
 
-        # Example for usage of ReleaseAPIClient
-        release_api_client = self.get_release_api_client()
+        # Additional examples for usage of ReleaseAPIClient
 
         # Step 1: Create a new global variable
         global_variable = {
