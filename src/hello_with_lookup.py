3189fa420ba75cd3c9022912aaea8ee465ab63f1
Balaji Venkatesan <balaji.venkatesan@digital.ai>
2025-10-08 10:37:08 +0530

    S-125312 Added lookup example task

---
+++ src/hello_with_lookup.py
@@ -0,0 +1,25 @@
+
+from digitalai.release.integration import BaseTask
+
+
+class HelloWithLookup(BaseTask):
+    """
+       Creates a greeting based on a name
+    """
+
+    def execute(self) -> None:
+
+        # Get the name from the input
+        name = self.input_properties['yourName']
+        if not name:
+            raise ValueError("The 'yourName' field cannot be empty")
+
+        # Create greeting
+        greeting = f"Hello {name}"
+
+        # Add greeting to the task's comment section in the UI
+        self.add_comment(greeting)
+
+        # Put greeting in the output of the task
+        self.set_output_property('greeting', greeting)
+
